ee38847a92d55229bc7b0b8ccf55b8730aadcae3
EstebanLuu <estebaanlunaaa@gmail.com>
2023-10-03 14:01:38 -0300

    feat:navbar enrutado responsive

--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,7 @@ import {
   HiEnvelope,
 } from 'react-icons/hi2';
 import Link from 'next/link';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
 // nav data
 export const navData = [
@@ -28,19 +28,38 @@ export const navData = [
   },
 ];
 
-
 const Nav = () => {
-  return <nav>
-    <div>
-      {
-       navData.map((ele, ind)=> 
-        <div key={ind}>
-          <Link href={ele.path}>{ele.icon}</Link>
-        </div>
-        ) 
-      }
-    </div>
-  </nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+    <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px]  xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full '>
+        {navData.map((ele, ind) => (
+          <div key={ind}>
+            <Link
+              className={`${
+                ele.path === pathname && 'text-accent'
+              } relative flex items-center group hover:text-accent transition-all duration-300`}
+              href={ele.path}
+            >
+              {/* tooltip */}
+              <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                <div className='bg-black opacity-50 relative flex text-white items-center p-[6px] rounded-[3px]'>
+                  <div className='text-[12px] leading-none font-semibold capitalize'>
+                    {ele.name}
+                  </div>
+                  <div className='border-solid border-l-black opacity-50 border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+                </div>
+              </div>
+              {/* triangle */}
+              {/* icon */}
+              <div className=''>{ele.icon}</div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
